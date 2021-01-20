5efb9666de8828e0bde46f98e34e033e0f70b502
iDv  <65097583+iDvloper@users.noreply.github.com>
2021-01-20 16:11:38 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Tienda de Camisetas</title>
+    </head>
+
+    <body>
+        <header id="header">
+            <div id="logo">
+                <img src="" alt="Camiseta logo">
+                <a href="index.php">
+                    <h1>
+                        Tienda Camisetas
+                    </h1>
+                </a>
+            </div>
+        </header>
+    </body>
+
+</html>
