d72fbad5a7ebaf5a5561233cb7f48ab92254bc82
iDv  <65097583+iDvloper@users.noreply.github.com>
2021-01-21 12:31:40 -0600

    update

--- index.php
+++ index.php
@@ -1,21 +1,86 @@
 <!DOCTYPE HTML>
 <html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <title>Tienda de Camisetas</title>
-    </head>
 
-    <body>
+<head>
+    <meta charset="utf-8">
+    <title>Tienda de Camisetas</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
         <header id="header">
             <div id="logo">
-                <img src="" alt="Camiseta logo">
+                <img src="assets/img/camiseta.jpg" alt="Camiseta logo">
                 <a href="index.php">
-                    <h1>
-                        Tienda Camisetas
-                    </h1>
+                    Tienda Camisetas
                 </a>
             </div>
         </header>
-    </body>
+
+        <nav id="menu">
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Categoria 1</a></li>
+                <li><a href="">Categoria 2</a></li>
+                <li><a href="">Categoria 3</a></li>
+            </ul>
+        </nav>
+
+        <div id="content">
+            <aside id="lateral">
+                <div id="login" class="block_aside">
+                    <form action="#" method="post">
+                        <label for="email">Email</label>
+                        <input type="email" name="email">
+
+                        <label for="password">Contraseña</label>
+                        <input type="password" name="password">
+
+                        <input type="submit" value="Enviar">
+                    </form>
+
+                    <a href="">Mis Pedidos</a>
+                    <a href="">Gestionar Pedidos</a>
+                    <a href="">Gestionar Categorias</a>
+                </div>
+            </aside>
+
+            <div id="central">
+                <div class="product">
+                    <img src="assets/img/camiseta.jpg" alt="Camiseta">
+                    <h2>Camiseta Azul</h2>
+                    <p>30 pesos</p>
+                    <a href="">Comprar</a>
+                </div>
+
+                <div class="product">
+                    <img src="assets/img/camiseta.jpg" alt="Camiseta">
+                    <h2>Camiseta Azul</h2>
+                    <p>30 pesos</p>
+                    <a href="">Comprar</a>
+                </div>
+
+                <div class="product">
+                    <img src="assets/img/camiseta.jpg" alt="Camiseta">
+                    <h2>Camiseta Azul</h2>
+                    <p>30 pesos</p>
+                    <a href="">Comprar</a>
+                </div>
+
+                <div class="product">
+                    <img src="assets/img/camiseta.jpg" alt="Camiseta">
+                    <h2>Camiseta Azul</h2>
+                    <p>30 pesos</p>
+                    <a href="">Comprar</a>
+                </div>
+            </div>
+        </div>
+
+        <footer id="footer">
+            <p>Ingeniero en Software Osvaldo Franco</p>
+        </footer>
+    </div>
+</body>
 
 </html>
